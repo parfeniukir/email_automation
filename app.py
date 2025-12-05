4a76c0e83bdda29cd806a2f4124ff5595810a61c
Ivanna Parfeniuk <parfeniuk.ivannaa@gmail.com>
2025-12-05 21:29:09 +0200

    Add functionality to the class

--- app.py
+++ app.py
@@ -1,6 +1,42 @@
+import requests
+from config import API_KEY
+
+
 class NewsFeed:
-    def __init__(self, data):
-        self.data = data
+    """Representing multiple news titles and links as a single string."""
+    base_url = "https://newsapi.org/v2/everything?"
+    search_in = "title,description"
+    api_url = API_KEY
+
+    def __init__(self, interests, from_date, to_date, language="en"):
+        self.interests = interests
+        self.from_date = from_date
+        self.to_date = to_date
+        self.language = language
 
     def get(self):
-        pass
+        url = (
+            f"{self.base_url}"
+            f"q={self.interests}&"
+            f"searchIn={self.search_in}&"
+            f"from={self.from_date}&"
+            f"to={self.to_date}&"
+            f"language={self.language}&"
+            f"apiKey={self.api_url}"
+        )
+
+        responce = requests.get(url)
+        content_dict = responce.json()
+        articles = content_dict["articles"]
+
+        email_body = ""
+        for article_dict in articles:
+            email_body = email_body + article_dict["title"] + "\n" + article_dict["url"] + "\n"*2
+
+
+new_feed = NewsFeed(interests="yoga", from_date="2025-12-04", to_date="2025-12-05", language="en")
+# print(new_feed.base_url)
+# print(new_feed.search_in)
+# print(new_feed.api_url)
+# print(new_feed.language)
+new_feed.get()
